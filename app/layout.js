b14e118a94dbbd7c49e83c0fe277c5b2ef6f3b17
randith-code <randithmadhusanka@gmail.com>
2024-04-10 21:05:18 +0530

    fix a bug

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,11 @@ export default function RootLayout({ children }) {
           href="http://www.beachsocial.leadmedia.lk/wp-content/uploads/2024/04/beach_social_logo.png"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
