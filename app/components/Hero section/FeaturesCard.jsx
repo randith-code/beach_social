b7889e8774ccaa728cdcb7eb8b00f80c1570a799
randith-code <randithmadhusanka@gmail.com>
2024-04-23 01:54:06 +0530

    UI fix

--- app/components/Hero section/FeaturesCard.jsx
+++ app/components/Hero section/FeaturesCard.jsx
@@ -4,7 +4,7 @@ export const FirstFeatureCard = ({ content }) => {
       <div className="w-1/2 aspect-square bg-main rounded-xl absolute bottom-0 left-0 -translate-x-1 translate-y-1"></div>
       <div className="flex flex-col items-center justify-center gap-4 bg-white drop-shadow w-full aspect-square rounded-xl">
         <img className="w-1/2 xl:w-1/3" src="/target.png" alt="feature image" />
-        <p className="text-center text-xs md:text-sm 2xl:text-xl font-semibold">
+        <p className="text-center text-xs md:text-sm 2xl:text-xl font-semibold w-3/4">
           Stratergic Social Media Planing
         </p>
       </div>
@@ -16,7 +16,7 @@ export const SecondFeaturesCard = ({ content }) => {
   return (
     <div className="flex flex-col items-center gap-4 justify-center bg-white drop-shadow w-full md:w-3/12 lg:w-3/12 aspect-square rounded-xl border-b-main border-b-4">
       <img className="w-1/2 xl:w-1/3" src="/ideas.png" alt="feature image" />
-      <p className="text-center text-xs md:text-sm 2xl:text-xl font-semibold">
+      <p className="text-center text-xs md:text-sm 2xl:text-xl font-semibold w-3/4">
         Stratergic Social Media Planing
       </p>
     </div>
@@ -27,7 +27,7 @@ export const ThirdFeatureCard = ({ content }) => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 bg-white drop-shadow w-full md:w-3/12 lg:w-3/12 aspect-square rounded-xl border-t-main border-t-4">
       <img className="w-1/2 xl:w-1/3" src="/stats.png" alt="feature image" />
-      <p className="text-center text-xs md:text-sm 2xl:text-xl font-semibold">
+      <p className="text-center text-xs md:text-sm 2xl:text-xl font-semibold w-3/4">
         Stratergic Social Media Planing
       </p>
     </div>
@@ -40,7 +40,7 @@ export const FourthFeatureCard = ({ content }) => {
       <div className="w-1/2 aspect-square bg-main rounded-xl absolute top-0 right-0 translate-x-1 -translate-y-1"></div>
       <div className="flex flex-col items-center justify-center gap-4 bg-white drop-shadow w-full aspect-square rounded-xl">
         <img className="w-1/2 xl:w-1/3" src="/people.png" alt="feature image" />
-        <p className="text-center text-xs md:text-sm 2xl:text-xl font-semibold">
+        <p className="text-center text-xs md:text-sm 2xl:text-xl font-semibold w-3/4">
           Stratergic Social Media Planing
         </p>
       </div>
