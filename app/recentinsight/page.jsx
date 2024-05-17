c4c08bf4baef8523be774c8afcd7c0c57ed27ca5
randith-code <randithmadhusanka@gmail.com>
2024-05-17 20:22:41 +0530

    UI fix

--- app/recentinsight/page.jsx
+++ app/recentinsight/page.jsx
@@ -1,7 +1,5 @@
 "use client";
-import { useState, useRef } from "react";
-import { Suspense } from "react";
-
+import { useState, useRef, Suspense } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
@@ -55,26 +53,31 @@ const RecentInsight = () => {
       ScrollTrigger.defaults({ scroller: scroller });
     });
   });
+
   return (
-    <Suspense>
-      <main ref={containerRef} className="h-screen">
-        <CustomNavbar />
-        <ParticlesComponent />
-        <div className=" absolute top-0 left-0 -z-50 bg-[url('/about_hero.png')] bg-cover bg-top w-full h-[80vh]" />
+    <main ref={containerRef} className="h-screen">
+      <CustomNavbar />
+      <ParticlesComponent />
+      <div className="absolute top-0 left-0 -z-50 bg-[url('/about_hero.png')] bg-cover bg-top w-full h-[80vh]" />
 
-        <div className="w-full flex justify-center">
-          <div className="w-3/4 py-28 flex flex-col gap-16">
-            <h1 className="font-Anton text-6xl">{searchParams.get("title")}</h1>
-            <img className="w-1/3" src={searchParams.get("img")} alt="" />
-            <p>{parse(searchParams.get("description"))}</p>
-          </div>
+      <div className="w-full flex justify-center">
+        <div className="w-3/4 py-28 flex flex-col gap-16">
+          <h1 className="font-Anton text-6xl">{searchParams.get("title")}</h1>
+          <img className="w-1/3" src={searchParams.get("img")} alt="" />
+          <p>{parse(searchParams.get("description"))}</p>
         </div>
+      </div>
 
-        <ContactUsPopUP open={popUpOpen} handelPopUpClose={handlePopUpClose} />
-        <Footer handlePopUpOpen={handlePopUpOpen} />
-      </main>
-    </Suspense>
+      <ContactUsPopUP open={popUpOpen} handelPopUpClose={handlePopUpClose} />
+      <Footer handlePopUpOpen={handlePopUpOpen} />
+    </main>
   );
 };
 
-export default RecentInsight;
+export default function WrappedRecentInsight() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RecentInsight />
+    </Suspense>
+  );
+}
